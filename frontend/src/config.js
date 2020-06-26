14816b53adfd30e738250a160b518d747ab36491
Francesco Venica <francescovenica@francescos-mbp.home>
2020-06-26 10:47:16 +0100

    fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // queste dovrebbero essere env settate in fase di build
 export default {
   API_URL: 'http://app.francescovenica.com/api',
-  WEBSOCKET_URL: 'wss://51.89.148.219/ws',
+  WEBSOCKET_URL: 'ws://51.89.148.219/ws',
 }
